f288e9b65905426c2662ea6b4e8540218f773be2
williamtran-tech <81273649+williamtran-tech@users.noreply.github.com>
2021-04-03 07:47:32 +0700

    Add files via upload

---
+++ BT9.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int row,i;
+	i=1;
+	printf("Input number of row: "); scanf("%d", &row);
+	int z;
+	z=row-2;
+	while (i<=row)
+	{
+			// row
+		printf("\n");
+		if (i==1 || i == row) {
+			for (int j=1; j<=row; j++){
+				printf("* ");
+			}
+		}
+			// column
+		else{ //if (i!=1 && i!=row)
+			printf("* ");
+			for (int j=1; j<=row; j++)
+			{
+				if (j!=1 && j!=row){
+					printf("  ");
+				}
+				else if (j==row){
+					printf("* ");
+				}
+			}
+		}
+		i++;
+	}
+	return 0;
+}
